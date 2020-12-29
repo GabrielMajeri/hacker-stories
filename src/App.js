3183d3bb811335cf2a0146e114df77b63b299212
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-12-29 18:26:40 +0200

    Use async/await syntax for data fetching

--- src/App.js
+++ src/App.js
@@ -64,18 +64,19 @@ const App = () => {
     setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
-  const fetchStories = useCallback(() => {
+  const fetchStories = useCallback(async () => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(url)
-      .then((response) => response.json())
-      .then((result) => {
-        dispatchStories({
-          type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
-        });
-      })
-      .catch(() => dispatchStories({ type: "STORIES_FETCH_FAILURE" }));
+    try {
+      const response = await fetch(url);
+      const result = await response.json();
+      dispatchStories({
+        type: "STORIES_FETCH_SUCCESS",
+        payload: result.hits,
+      });
+    } catch {
+      dispatchStories({ type: "STORIES_FETCH_FAILURE" });
+    }
   }, [url]);
 
   useEffect(() => {
